07a019b37d1814932c5e73286194f81c29be2051
PramahadiTamaPutra <pramahaditamaputra@outlook.com>
2021-01-22 16:55:04 +0700

    Fix

--- src/ListSoal.js
+++ src/ListSoal.js
@@ -10,21 +10,9 @@ const ListSoal = ({ datas }) => {
             <div className="mb-3" key={data.id}>
               <Soal soal={data.soal} />
               <Jawaban tipe={data.type} pilihan={data.a} stateCheck={true} />
-              <Jawaban
-                tipe={data.type}
-                pilihan={data.b}
-                jawaban={data.jawaban}
-              />
-              <Jawaban
-                tipe={data.type}
-                pilihan={data.c}
-                jawaban={data.jawaban}
-              />
-              <Jawaban
-                tipe={data.type}
-                pilihan={data.d}
-                jawaban={data.jawaban}
-              />
+              <Jawaban tipe={data.type} pilihan={data.b} />
+              <Jawaban tipe={data.type} pilihan={data.c} />
+              <Jawaban tipe={data.type} pilihan={data.d} />
             </div>
           );
         } else if (data.id === 2) {
